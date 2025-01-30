c06a66c4af3e46c6c69b58f085745bdd45ba05c3
Vinay Shetye <vinay.courses.official@gmail.com>
2025-01-30 11:23:43 -0500

    updated file - added sample feedbacks

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -2,18 +2,53 @@ const FeedbackData = [
   {
     id: 1,
     rating: 10,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    text: "Absolutely love the user interface! It’s intuitive and makes navigating through the app a breeze.",
   },
   {
     id: 2,
     rating: 9,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quaes.',
+    text: "Great features and reliable performance. Would love to see integration with more third-party services.",
   },
   {
     id: 3,
     rating: 8,
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quaez.',
+    text: "The application is very useful, but the loading times could be improved for a smoother experience.",
   },
-]
+  {
+    id: 4,
+    rating: 7,
+    text: "Good functionality overall, but I encountered some bugs during the last update that need addressing.",
+  },
+  {
+    id: 5,
+    rating: 9,
+    text: "Excellent customer support! They resolved my issues promptly and were very helpful.",
+  },
+  {
+    id: 6,
+    rating: 6,
+    text: "The app meets my basic needs, but adding more customization options would enhance usability.",
+  },
+  {
+    id: 7,
+    rating: 10,
+    text: "Outstanding performance and sleek design. Highly recommend to anyone looking for this type of solution.",
+  },
+  {
+    id: 8,
+    rating: 8,
+    text: "User-friendly and feature-rich, though the mobile version could use some optimization.",
+  },
+  {
+    id: 9,
+    rating: 7,
+    text: "Solid application with potential. Looking forward to new updates and features in the future.",
+  },
+  {
+    id: 10,
+    rating: 9,
+    text: "Very satisfied with the recent updates. The new dashboard layout is particularly impressive.",
+  },
+];
 
-export default FeedbackData
+export default FeedbackData;
